6e1d9e8650d2cb6e5efbf63853fe5a248b333c5a
AditiShree <aditi@kutsix.com>
2016-07-21 13:35:41 +0530

    1st commit

---
+++ main.go
@@ -0,0 +1,73 @@
+package main  
+
+import (
+	"net/http"
+	"os"
+	"text/template"
+	"io"
+	"fmt"
+	"time"
+)
+
+const STATIC_URL string = "/public/"
+const STATIC_ROOT string = "public/"
+
+func populateTemplate() *template.Template  {
+	result := template.New("templates")
+
+	basePath := "views"
+
+	templateFolder,_ := os.Open(basePath)
+	defer templateFolder.Close()
+
+	templatePathRaw,_ := templateFolder.Readdir(-1)
+
+	templatePaths := new([]string)
+
+	for _,pathInfo := range templatePathRaw {
+		if !pathInfo.IsDir() {
+			*templatePaths = append(*templatePaths, basePath + "/" + pathInfo.Name())
+		}
+	}
+
+	result.ParseFiles(*templatePaths...)
+
+	return result
+}
+
+func StaticHandler(w http.ResponseWriter, req *http.Request) {
+	static_file := req.URL.Path[len(STATIC_URL):]
+	fmt.Println("Static file: ", static_file)
+
+	if len(static_file) != 0 {
+		f, err := http.Dir(STATIC_ROOT).Open(static_file)
+		if err==nil {
+			content := io.ReadSeeker(f)
+			http.ServeContent(w, req, static_file, time.Now(), content)
+			return 
+		}
+	}
+	http.NotFound(w, req)
+}
+
+func main() {
+	templates := populateTemplate()
+	http.HandleFunc("/",
+		func (w http.ResponseWriter, req *http.Request) {
+			requestedFile := req.URL.Path[1:]
+			template := templates.Lookup(requestedFile + ".html")
+			fmt.Println("template:", template)
+
+			if template != nil {
+				template.Execute(w, nil)
+			}else{
+				w.WriteHeader(404)
+			}
+		})
+
+	http.HandleFunc(STATIC_URL, StaticHandler)
+
+	//http.HandleFunc("/index", func(w))
+
+	http.ListenAndServe(":9000", nil)
+}
---
+++ public/js/home.js
@@ -0,0 +1,33 @@
+window.onload = init;
+
+function init() {
+	backToTop();
+}
+
+function backToTop() {
+	// Only enable if the document has a long scroll bar
+	// Note the window height + offset
+	if ( ($(window).height() + 100) < $(document).height() ) {
+	    $('#top-link-block').removeClass('hidden').affix({
+	        // how far to scroll down before link "slides" into view
+	        offset: {top:100}
+	    });
+	}
+	/*$(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');*/
+}
